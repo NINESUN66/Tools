07be40fd67c1462c10a2c87cbccb0d1f6b51a86c
NINESUN66 <2931178847@qq.com>
2025-02-24 16:28:54 +0800

    Update YuanshenHelper.bat

---
+++ yuanshenHelper.bat
@@ -0,0 +1,7 @@
+E:/miniconda3/envs/ys/python.exe e:/Pycharm/MihoyoBBSTools/main.py
+pause
+
+# 原神自动签到项目地址：
+# https://github.com/Womsxd/MihoyoBBSTools
+# 米游社Stoken获取项目地址：
+# https://github.com/HolographicHat/GetToken
